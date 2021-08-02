a264ddfcae85188ca82981ae2071b32ca3092501
Colin Smith <smith.colin00@gmail.com>
2021-08-01 18:33:32 -0600

    aded account

--- PrejittedLambda.Infrastructure/Program.cs
+++ PrejittedLambda.Infrastructure/Program.cs
@@ -7,7 +7,14 @@
         public static void Main(string[] args)
         {
             var app = new App();
-            _ = new MainStack(app, "PrejittedLambda", new StackProps());
+            _ = new MainStack(app, "PrejittedLambda", new StackProps 
+            {
+                Env = new Environment
+                {
+                    Account = "685696558467",
+                    Region = "us-west-2"
+                }
+            });
             app.Synth();
         }        
     }
